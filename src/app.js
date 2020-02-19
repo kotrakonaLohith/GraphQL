38d2fb7102087bfb3bfecdb55ec1268b610d7338
Lohith Kotrakona <Lohith.Kotrakona@rotary.org>
2020-02-19 10:36:08 -0600

    added nodemon package to save and restart the server automatically and tried some queries on my own

--- src/app.js
+++ src/app.js
@@ -1,4 +1,77 @@
-import addition, { sub } from "./math";
-console.log(addition(10, 3));
+import { GraphQLServer } from "graphql-yoga";
 
-console.log(sub(10, 5));
+// type definitions
+// string, Boolean, Int, Float, ID (scalar types)
+
+const typeDefs = `
+type Query {
+    hello: String!
+    name: String!
+    location: String!
+    bio: String!
+    id: ID!
+    age: Int!
+    employed: Boolean!
+    gpa: Float
+    title: String!
+    price: Float!
+    releaseYear: Int
+    rating: Float
+    inStock: Boolean!
+}
+`;
+
+// resolvers
+
+const resolvers = {
+  Query: {
+    hello() {
+      return "This is my first query";
+    },
+    name() {
+      return "Lohith";
+    },
+    location() {
+      return "chicago,Illinois";
+    },
+    bio() {
+      return "I'm a Front end developer";
+    },
+    id() {
+      return "asdasdasdrbvcdfv1234";
+    },
+    age() {
+      return 26;
+    },
+    employed() {
+      return true;
+    },
+    gpa() {
+      return 3.3;
+    },
+    title() {
+      return "stickers";
+    },
+    price() {
+      return 3.56;
+    },
+    releaseYear() {
+      return 2021;
+    },
+    rating() {
+      return null;
+    },
+    inStock() {
+      return true;
+    }
+  }
+};
+
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers
+});
+
+server.start(() => {
+  console.log("the server is up");
+});
--- src/myModule.js
+++
@@ -1,7 +0,0 @@
-const name = "lohith";
-const message = "some text from this module";
-
-const greeting = name => {
-  return ` hi ${name}`;
-};
-export { message, greeting, name as default };
