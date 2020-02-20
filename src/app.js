b4a0f598340dbe0c05bdd8e13431127d6d85de6b
lkotrakona <lohith.kotrakona@rotary.org>
2020-02-20 11:37:57 -0600

    practised some relational data concepts between queries in graphql

--- src/app.js
+++ src/app.js
@@ -1,165 +1,129 @@
-import {GraphQLServer} from "graphql-yoga";
+import { GraphQLServer } from 'graphql-yoga'
 
-// type definitions
-// string, Boolean, Int, Float, ID (scalar types)
+// Scalar types - String, Boolean, Int, Float, ID
 
-const users = [
-  {
-    id: "1",
-    name: "lohith",
-    email: "lohithr516@gmail.com",
+// Demo user data
+const users = [{
+    id: '1',
+    name: 'Andrew',
+    email: 'andrew@example.com',
     age: 27
-  },
-  {
-    id: "2",
-    name: "Rohith",
-    email: "rohith09@gmail.com",
-    age: 23
-  },
-  {
-    id: "3",
-    name: "neethu",
-    email: "neethu10@gmail.com",
-    age: 22
-  }
-];
+}, {
+    id: '2',
+    name: 'Sarah',
+    email: 'sarah@example.com'
+}, {
+    id: '3',
+    name: 'Mike',
+    email: 'mike@example.com'
+}]
 
+const posts = [{
+    id: '10',
+    title: 'GraphQL 101',
+    body: 'This is how to use GraphQL...',
+    published: true,
+    author: '1'
+}, {
+    id: '11',
+    title: 'GraphQL 201',
+    body: 'This is an advanced GraphQL post...',
+    published: false,
+    author: '2'
+}, {
+    id: '12',
+    title: 'Programming Music',
+    body: '',
+    published: false,
+    author: '2'
+}]
+
+// Type definitions (schema)
 const typeDefs = `
-type Query {
-    hello: String!
-    name: String!
-    location: String!
-    bio: String!
-    id: ID!
-    age: Int!
-    employed: Boolean!
-    gpa: Float
-    title: String!
-    price: Float!
-    releaseYear: Int
-    rating: Float
-    inStock: Boolean!
-    me: User!
-    post: Post!
-    greeting(name: String!): String!
-    add(numbers:[Float!]!): Float!
-    grades:[Int!]!
-    users(query:String!):[User!]!
-}
-type User {
-    id: ID!
-    name: String!
-    email: String!
-    age: Int
-}
-type Post {
-    id:ID!
-    title: String!
-    body: String!
-    published: Boolean!
-}
-`;
-
-// resolvers
-
-const resolvers = {
-  Query: {
-    greeting(parent, args, ctx, info) {
-      console.log(args);
-      if (args.name) {
-        return `Hello ${args.name}`;
-      } else {
-        return "Hello";
-      }
-    },
-
-    users(parent, args, ctx, info) {
-      if (args.query) {
-        return users;
-      } else {
-        return users.filter(user => {
-          return user.name.toLowerCase().includes(args.query.toLowerCase());
-        });
-      }
-    },
-
-    add(parent, args, ctx, info) {
-      if (args.numbers.length === 0) {
-        return 0;
-      }
-      // reduce take in 2 args "acc" as 1st value and the adjacent value as 2nd arg
-      return args.numbers.reduce((acc, currentVal) => {
-        return acc + currentVal;
-      });
-    },
-
-    grades(parent, args, ctx, info) {
-      return [99, 80, 93];
-    },
-
-    hello() {
-      return "This is my first query";
-    },
-    name() {
-      return "Lohith";
-    },
-    location() {
-      return "chicago,Illinois";
-    },
-    bio() {
-      return "I'm a Front end developer";
-    },
-    id() {
-      return "asdasdasdrbvcdfv1234";
-    },
-    age() {
-      return 26;
-    },
-    employed() {
-      return true;
-    },
-    gpa() {
-      return 3.3;
-    },
-    title() {
-      return "stickers";
-    },
-    price() {
-      return 3.56;
-    },
-    releaseYear() {
-      return 2021;
-    },
-    rating() {
-      return null;
-    },
-    inStock() {
-      return true;
-    },
-    me() {
-      return {
-        id: "sdfdsfsd3e5423423",
-        name: "Kotrakona Lohith Reddy",
-        email: "lohithr516@gmail.com",
-        age: 26
-      };
-    },
-    post() {
-      return {
-        id: "asdgashjdgasdhjadsjhasdja213216732163",
-        title: "My Blog",
-        body: "This is my first post",
-        published: true
-      };
+    type Query {
+        users(query: String): [User!]!
+        posts(query: String): [Post!]!
+        me: User!
+        post: Post!
     }
-  }
-};
+
+    type User {
+        id: ID!
+        name: String!
+        email: String!
+        age: Int
+        posts: [Post!]!
+    }
+
+    type Post {
+        id: ID!
+        title: String!
+        body: String!
+        published: Boolean!
+        author: User!
+    }
+`
+
+// Resolvers
+const resolvers = {
+    Query: {
+        users(parent, args, ctx, info) {
+            if (!args.query) {
+                return users
+            }
+
+            return users.filter((user) => {
+                return user.name.toLowerCase().includes(args.query.toLowerCase())
+            })
+        },
+        posts(parent, args, ctx, info) {
+            if (!args.query) {
+                return posts
+            }
+
+            return posts.filter((post) => {
+                const isTitleMatch = post.title.toLowerCase().includes(args.query.toLowerCase())
+                const isBodyMatch = post.body.toLowerCase().includes(args.query.toLowerCase())
+                return isTitleMatch || isBodyMatch
+            })
+        },
+        me() {
+            return {
+                id: '123098',
+                name: 'Mike',
+                email: 'mike@example.com'
+            }
+        },
+        post() {
+            return {
+                id: '092',
+                title: 'GraphQL 101',
+                body: '',
+                published: false
+            }
+        }
+    },
+    Post: {
+        author(parent, args, ctx, info) {
+            return users.find((user) => {
+                return user.id === parent.author
+            })
+        }
+    },
+    User:{
+      posts(parent,args,ctx,info){
+        return posts.filter((post)=>{
+          return post.author === parent.id
+        })
+      }
+    }
+}
 
 const server = new GraphQLServer({
-  typeDefs,
-  resolvers
-});
+    typeDefs,
+    resolvers
+})
 
 server.start(() => {
-  console.log("the server is up");
-});
+    console.log('The server is up!')
+})
